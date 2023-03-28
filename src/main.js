12a420d8a748a2a7875925537e05b7c775b9c1a7
milkkkky <2206002917@qq.com>
2023-03-28 10:24:34 +0800

    展示场景

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-console.log(THREE);
 
-/**
+/*
 场景：Scene
 相机：Camera
 渲染器：Renderer
@@ -11,31 +10,46 @@ console.log(THREE);
 灯光：Light
 */
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(66, 66, 66);
 const camera = new THREE.PerspectiveCamera(
-  45,
+  80,
   window.innerWidth / window.innerHeight,
   0.01,
-  50
+  200
 );
-camera.position.set(0, 0, 5);
+camera.position.set(20, 10, 20);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+//环境光
+const ambientLight = new THREE.AmbientLight({ color: 0xaaaaaa });
+scene.add(ambientLight);
+
+const directionLight = new THREE.DirectionalLight({ color: 0xffffff });
+scene.add(directionLight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-// const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
 // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 // scene.add(boxMesh);
 
 const gltfLoader = new GLTFLoader();
-
 gltfLoader.load("scene.glb", (gltf) => {
   scene.add(gltf.scene);
 });
-boxMesh.position.set(-2, 0, 0);
-setInterval(() => {
+
+animate();
+function animate() {
+  requestAnimationFrame(animate);
   renderer.render(scene, camera);
   controls.update();
-}, 1000 / 24);
+}
+
+// setInterval(()=>{
+//     renderer.render(scene, camera);
+//     controls.update();
+// }, 1000 / 24)
+
+// boxMesh.position.set(-2, 0, 0);
